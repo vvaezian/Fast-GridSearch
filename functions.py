356b7ebd268717067ffbeaa5371c7de1859836a4
Vahid <vahid.vaezian@gmail.com>
2021-10-07 22:46:25 -0700

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,28 @@
+def get_params(model):
+  
+  from inspect import getsource
+  doc = getsource(DecisionTreeRegressor)
+  
+  param_lines = []
+  for row in doc.split('\n'):
+    if ':' in row and row.startswith(' ') and 'default' in row.lower():
+      param_lines.append(row)
+    if 'attribute' in row.lower():
+      break
+  
+  from collections import defaultdict
+  param_names = defaultdict(dict)
+  for param_line in param_lines:
+    param_name = param_line.split(':')[0].strip()
+    if '{' in param_line and '}' in param_line:
+      #values = param_line.split('{')[1].split('}')[0].replace('"', '').split(',')
+      import re
+      values = eval(re.search('.*(\{.*?\}).*', a).group(1))
+      param_names[param_name] = values
+    else:
+      param_names[param_name] = []
+    
+  return param_names
+
+# from sklearn.tree import DecisionTreeRegressor
+# get_params(DecisionTreeRegressor)
